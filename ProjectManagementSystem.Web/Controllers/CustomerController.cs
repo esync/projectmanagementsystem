34f5f91ef5e829231c210a4a27cb20bd1b02c1d1
Apollo.Kariuki <apollo.kariuki@equitybank.co.ke>
2016-03-30 19:13:31 +0300

    Creation of REST API Services

---
+++ ProjectManagementSystem.Web/Controllers/CustomerController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ProjectManagementSystem.Web.Models;
+
+namespace ProjectManagementSystem.Web.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        private PmSyncDbContext db = new PmSyncDbContext();
+
+        // GET: api/Customer
+        public IQueryable<Customer> GetCustomers()
+        {
+            return db.Customers;
+        }
+
+        // GET: api/Customer/5
+        [ResponseType(typeof(Customer))]
+        public async Task<IHttpActionResult> GetCustomer(int id)
+        {
+            Customer customer = await db.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(customer);
+        }
+
+        // PUT: api/Customer/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutCustomer(int id, Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != customer.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(customer).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CustomerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Customer
+        [ResponseType(typeof(Customer))]
+        public async Task<IHttpActionResult> PostCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Customers.Add(customer);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = customer.Id }, customer);
+        }
+
+        // DELETE: api/Customer/5
+        [ResponseType(typeof(Customer))]
+        public async Task<IHttpActionResult> DeleteCustomer(int id)
+        {
+            Customer customer = await db.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            db.Customers.Remove(customer);
+            await db.SaveChangesAsync();
+
+            return Ok(customer);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CustomerExists(int id)
+        {
+            return db.Customers.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ ProjectManagementSystem.Web/Controllers/ProjectController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ProjectManagementSystem.Web.Models;
+
+namespace ProjectManagementSystem.Web.Controllers
+{
+    public class ProjectController : ApiController
+    {
+        private PmSyncDbContext db = new PmSyncDbContext();
+
+        // GET: api/Project
+        public IQueryable<Project> GetProjects()
+        {
+            return db.Projects;
+        }
+
+        // GET: api/Project/5
+        [ResponseType(typeof(Project))]
+        public async Task<IHttpActionResult> GetProject(int id)
+        {
+            Project project = await db.Projects.FindAsync(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(project);
+        }
+
+        // PUT: api/Project/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutProject(int id, Project project)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != project.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(project).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProjectExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Project
+        [ResponseType(typeof(Project))]
+        public async Task<IHttpActionResult> PostProject(Project project)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Projects.Add(project);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = project.Id }, project);
+        }
+
+        // DELETE: api/Project/5
+        [ResponseType(typeof(Project))]
+        public async Task<IHttpActionResult> DeleteProject(int id)
+        {
+            Project project = await db.Projects.FindAsync(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            db.Projects.Remove(project);
+            await db.SaveChangesAsync();
+
+            return Ok(project);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ProjectExists(int id)
+        {
+            return db.Projects.Count(e => e.Id == id) > 0;
+        }
+    }
+}
