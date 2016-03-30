ff9a5d533ff84937854124c826bdeeb1d316bee6
Apollo.Kariuki <apollo.kariuki@equitybank.co.ke>
2016-03-30 18:54:34 +0300

    Creation of Database Tables

---
+++ ProjectManagementSystem.Web/App_Data/DBScripts/03_Projects.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Projects]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ProjectName] NVARCHAR(256) NOT NULL, 
+    [CustomerId] INT NOT NULL, 
+    [EmployeeId] INT NOT NULL, 
+    [StartDate] DATETIME NOT NULL, 
+    [EndDate] DATETIME NOT NULL, 
+    [Comments] NVARCHAR(MAX) NULL, 
+    [Attachment] VARBINARY(MAX) NULL, 
+    CONSTRAINT [FK_Projects_Customers] FOREIGN KEY ([CustomerId]) REFERENCES [Customers]([Id]), 
+    CONSTRAINT [FK_Projects_Employees] FOREIGN KEY ([EmployeeId]) REFERENCES [Employees]([Id])
+)
