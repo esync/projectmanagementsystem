8a9f235da7b03309ebde46a4c2620cfca6709c6c
kevin_nganga <knnjoroge@gmail.com>
2016-04-06 17:12:02 +0300

    Added to .SPA

---
+++ ProjectManagementSystem.SPA/app/scripts/app.js
@@ -0,0 +1,39 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('app', [
+        // Angular modules 
+        'ui.router',
+        'ngAnimate',
+        'ngCookies',
+        'ngMessages',
+        'ngResource',
+        'ngSanitize'
+
+        // Custom modules 
+
+        // 3rd Party Modules
+        
+    ])
+    .config(function ($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise("/login");
+
+        $stateProvider
+
+          .state('login', {
+              url: '/login',
+              templateUrl: '../views/login/authenticate.html',
+              controller: 'authenticate'
+          })
+
+          .state('dashboard', {
+              url: '/dashboard',
+              templateUrl: 'views/dashboard.html',
+              controller: 'dashboard'
+          })
+
+          
+
+    });
+})();
---
+++ ProjectManagementSystem.SPA/app/scripts/controller/dashboard.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('dashboard', dashboard);
+
+    dashboard.$inject = ['$location']; 
+
+    function dashboard($location) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'dashboard';
+
+        activate();
+
+        function activate() { }
+    }
+})();
