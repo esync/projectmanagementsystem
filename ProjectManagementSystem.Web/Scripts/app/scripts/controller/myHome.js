cf4e14501d0f65c6c481a4bdf9cfe9ddb0ae2aaa
kevin_nganga <knnjoroge@gmail.com>
2016-04-06 09:16:59 +0300

    UI Additions

---
+++ ProjectManagementSystem.Web/Scripts/app/scripts/controller/myHome.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('myHome', myHome);
+
+    myHome.$inject = ['$scope']; 
+
+    function myHome($scope) {
+        $scope.title = 'myHome';
+
+        activate();
+
+        function activate() { }
+    }
+})();
