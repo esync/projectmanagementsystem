ebd92b77dad417ce2de655eea996a35ee9a86c52
Njuguna.Samuel <captahsyn@gmail.com>
2016-04-09 15:02:57 +0300

    Added the confirm password field in Employee and Customer creation

--- ProjectManagementSystem.Web/ViewModels/CustomerModel.cs
+++ ProjectManagementSystem.Web/ViewModels/CustomerModel.cs
@@ -24,12 +24,20 @@ namespace ProjectManagementSystem.Web.ViewModels
         [Display(Name = "User Name")]
         public string UserName { get; set; }
 
+        [Required]
         [Display(Name = "Email")]
         [EmailAddress]
         public string Email { get; set; }
 
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [Display(Name = "Password")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
     }
 }
--- ProjectManagementSystem.Web/ViewModels/EmployeeModel.cs
+++ ProjectManagementSystem.Web/ViewModels/EmployeeModel.cs
@@ -21,6 +21,7 @@ namespace ProjectManagementSystem.Web.ViewModels
         [Display(Name = "User Name")]
         public string UserName { get; set; }
 
+        [Required]
         [Display(Name = "Email")]
         [EmailAddress]
         public string Email { get; set; }
@@ -28,9 +29,16 @@ namespace ProjectManagementSystem.Web.ViewModels
         [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
 
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [Display(Name = "Password")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
 
         [Display(Name = "Role")]
         public string Role { get; set; }
